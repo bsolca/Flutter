26db0f830ea342dc121fb0f2103786c057ffeb68
Benjamin Solca <benjamin@solca.io>
2020-07-26 19:06:25 +0200

    [~] change to SingleChildScrollView <> [+] Description input

--- shop_app/lib/pages/edit_product_page.dart
+++ shop_app/lib/pages/edit_product_page.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 
 class EditProductPage extends StatefulWidget {
   static const routeName = '/edit-product';
+
   @override
   _EditProductPageState createState() => _EditProductPageState();
 }
 
 class _EditProductPageState extends State<EditProductPage> {
   final _priceFocusNode = FocusNode();
+  final _priceDescriptionNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,22 +20,34 @@ class _EditProductPageState extends State<EditProductPage> {
       body: Form(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: ListView(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Title'),
-                textInputAction: TextInputAction.next,
-                onFieldSubmitted: (_) {
-                  FocusScope.of(context).requestFocus(_priceFocusNode);
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Price'),
-                textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.number,
-                focusNode: _priceFocusNode,
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Title'),
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_priceFocusNode);
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Price'),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_priceDescriptionNode);
+                  },
+                  focusNode: _priceFocusNode,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Description'),
+                  maxLines: 3,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.text,
+                  focusNode: _priceDescriptionNode,
+                ),
+              ],
+            ),
           ),
         ),
       ),
